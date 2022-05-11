a27775540a1d25f032efe26f23d7843fbb0358d9
dnstock <daniel.ht@gmail.com>
2022-05-11 14:06:48 -0400

    Update app router to use new assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import withRoot from "./modules/withRoot";
+
+import Home from "./Home";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Terms from "./Terms";
+import Privacy from "./Privacy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/terms" element={<Terms/>} />
+          <Route path="/privacy" element={<Privacy/>} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default withRoot(App);
